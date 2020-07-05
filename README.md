d2a79fb57412187424474179549625e7faa8735e
Manish Singh <55324405+manish080883@users.noreply.github.com>
2020-07-05 01:15:18 +0100

    Started publishing messages to kafka using sprint cloud stream

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rsvpApplication
+
+# To start Zookeeper
+C:\work\apache\zookeeper\apache-zookeeper-3.6.1-bin\bin>zkserver
+
+# To start the kafka server
+C:\work\apache\kafka\kafka_2.13-2.5.0\bin\windows>kafka-server-start.bat ..\..\config\server.properties
+
+# To start a sample consumer on a topic
+C:\work\apache\kafka\kafka_2.13-2.5.0\bin\windows>kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic meetupTopic --from-beginning
  
--- src/main/java/com/maniss/RSVPCollector/RSVPWebSocketHandler.java
+++ src/main/java/com/maniss/RSVPCollector/RSVPWebSocketHandler.java
@@ -5,13 +5,26 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Component
 public class RSVPWebSocketHandler extends AbstractWebSocketHandler
 {
+    private static final Logger logger = Logger.getLogger(RSVPWebSocketHandler.class.getName());
+
+    private final RsvpKafkaProducer rsvpKafkaProducer;
+
+    public RSVPWebSocketHandler(RsvpKafkaProducer rsvpKafkaProducer) {
+        this.rsvpKafkaProducer = rsvpKafkaProducer;
+    }
+
     @Override
     public void handleMessage(WebSocketSession session,
                               WebSocketMessage<?> message)
     {
-        System.out.println(message.getPayload());
+       logger.log(Level.INFO, "New RSVP:\n {0}", message.getPayload());
+
+       rsvpKafkaProducer.sendRsvpMessage(message);
     }
 }
